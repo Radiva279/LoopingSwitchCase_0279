424aecbe0d320a181bfc6c8ae7f0a3987ecfca0c
Radiva279 <radivagalih12@gmail.com>
2025-03-18 11:14:22 +0700

    perulangan for pencacah naik dan turun

--- file1.cpp
+++ file1.cpp
@@ -5,11 +5,15 @@ int main(){
     int cacah;
     // perulangan cacah for untuk pencacah naik
     for(cacah = 0; cacah <= 4; cacah++){
-        cout << "Nilai Pencacah = " << cach << " Selamat Datang!" << endl;
+        cout << "Nilai Pencacah = " << cacah << " Selamat Datang!" << endl;
     }
-    cout << "Nilai pencaah terakhir = " << cacah;
+    cout << "Nilai pencacah terakhir = " << cacah;
     cout << endl;
     cout << endl;
 
-    //perulangan for untuk pencacah turun
+    // perulangan for untuk pencacah turun
+    for(cacah = 5; cacah > 0; cacah--){
+        cout << "Nilai Pencacah =" << cacah << "Selamat Datang!" << endl;
+    }
+    cout << "Nilai pencacah terakhir = " << cacah;
 }
