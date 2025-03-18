605a024947534e0b8ba918c1a57b581732c2ff77
Radiva279 <radivagalih12@gmail.com>
2025-03-18 10:56:46 +0700

    first commit

---
+++ file1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int cacah;
+    // perulangan cacah for untuk pencacah naik
+    for(cacah = 0; cacah <= 4; cacah++){
+        cout << "Nilai Pencacah = " << cach << " Selamat Datang!" << endl;
+    }
+    cout << "Nilai pencaah terakhir = " << cacah;
+    cout << endl;
+    cout << endl;
+
+    //perulangan for untuk pencacah turun
+}
